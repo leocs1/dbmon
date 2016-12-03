88626b125fa3069cc210269a35833763b9f9cb4b
Leo <lcarlosdasilva@hotmail.com>
2016-12-03 13:39:27 -0200

    teste inicial do github

---
+++ djangoecommerce/dir.py
@@ -0,0 +1,9 @@
+
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+print(BASE_DIR)
+print(PROJECT_ROOT)
+
