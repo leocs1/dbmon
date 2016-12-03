104f2dc34da29858c113799bbc4ceceb924c17e9
Leo <lcarlosdasilva@hotmail.com>
2016-12-03 13:52:53 -0200

    Revert "teste inicial do github"
    This reverts commit 88626b125fa3069cc210269a35833763b9f9cb4b.

--- djangoecommerce/dir.py
+++
@@ -1,9 +0,0 @@
-
-
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-print(BASE_DIR)
-print(PROJECT_ROOT)
-
